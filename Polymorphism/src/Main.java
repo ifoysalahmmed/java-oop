d9e9eb5057d1e1390565db9d36327d4838238988
Foysal Ahmmed Limon <ifoysalahmmed@gmail.com>
2024-05-08 21:40:37 +0600

    added Polymorphism

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,5 +1,28 @@
+class Student {
+    String name;
+    int age;
+
+    public void printInfo(String name) {
+        System.out.println(name);
+    }
+
+    public void printInfo(int age) {
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age) {
+        System.out.println(name + " " + age);
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student = new Student();
+        student.name = "John";
+        student.age = 23;
+        student.printInfo(student.name);
+        student.printInfo(student.age);
+        student.printInfo(student.name, student.age);
     }
 }
